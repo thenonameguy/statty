f5d95282040759b3f44f185f5cba5581c5a2dc57
Krisztián Szabó <thenonameguy24@gmail.com>
2013-06-01 23:02:19 +0200

    Simplified code by removing unnecesary constructors, gofmt'd

--- main.go
+++ main.go
@@ -1,61 +1,53 @@
 package main
 
 import (
+	"encoding/json"
+	"flag"
 	"fmt"
-  "github.com/jondot/gosigar"
-  "encoding/json"
-  "net/http"
-  "flag"
+	"github.com/jondot/gosigar"
+	"log"
+	"net/http"
 )
 
-type InfoHolder struct{
-  Uptime sigar.Uptime
-  Cpus sigar.CpuList
-  FileSystems sigar.FileSystemList
-  Swap sigar.Swap
-  Mem sigar.Mem
+type InfoHolder struct {
+	Uptime sigar.Uptime
+	Cpus   sigar.CpuList
+	Swap   sigar.Swap
+	Mem    sigar.Mem
 }
 
-func  NewInfoHolder() *InfoHolder{
-  i:=new(InfoHolder)
-  i.Uptime=sigar.Uptime{}
-  i.Cpus=sigar.CpuList{}
-  i.FileSystems=sigar.FileSystemList{}
-  i.Swap=sigar.Swap{}
-  i.Mem=sigar.Mem{}
-  return i
+func (i *InfoHolder) Update() {
+	i.Uptime.Get()
+	i.Cpus.Get()
+	i.Swap.Get()
+	i.Mem.Get()
 }
 
-func (i *InfoHolder)Update(){
-  i.Uptime.Get()
-  i.Cpus.Get()
-  i.FileSystems.Get()
-  i.Swap.Get()
-  i.Mem.Get()
+func (i *InfoHolder) String() string {
+	b, err := json.Marshal(i)
+	if err != nil {
+		return ""
+	}
+	return string(b)
 }
 
-func (i *InfoHolder) String() string{
-  b,err:=json.Marshal(i)
-  if err!=nil{
-    return ""
-  }
-  return string(b)
+func DataServer(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	info.Update()
+	fmt.Fprint(w, info)
 }
 
-func DataServer(w http.ResponseWriter,req *http.Request){
-    w.Header().Set("Content-Type", "application/json")
-    info.Update()
-    fmt.Fprint(w,info)
-}
-
-var info=NewInfoHolder()
+var info = new(InfoHolder)
 var port string
 
 func main() {
-  flag.StringVar(&port,"port",":8080","Statty's port address")
-  flag.Parse()
-  fmt.Println("Listening on port",port)
-  http.Handle("/", http.FileServer(http.Dir("./web/")))
-  http.HandleFunc("/data",DataServer)
-  http.ListenAndServe(port,nil)
+	flag.StringVar(&port, "port", ":8080", "Statty's port address")
+	flag.Parse()
+	fmt.Println("Listening on port", port)
+	http.Handle("/", http.FileServer(http.Dir("./web/")))
+	http.HandleFunc("/data", DataServer)
+	err := http.ListenAndServe(port, nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
